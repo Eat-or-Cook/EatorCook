40c4a05617e86f20e338e56bc1e73fba1665f19a
jetly sandita <jetly27sandita@gmail.com>
2020-02-07 08:44:50 +0700

    add google map api on client side add function detail zomato

--- client/script/zomato.js
+++ client/script/zomato.js
@@ -13,10 +13,83 @@ var data = []
   
   
   function closeDetail(){
-    // $zomato.show()
-    // $google.hide()
+    $zomato.show()
+    $google.hide()
   }
   ////
+  function detail(id){
+    $zomato.hide()
+    $google.show()
+    $.ajax(`http://localhost:3001/zomato/${id}`,{
+      method: "GET"
+    })
+    .done(function(result){
+      console.log(result)
+      var template = `
+                        <div class="card text-white bg-danger" >
+                          <img class="card-img" src="" alt="Card image cap" style="height:30vh;">
+                          <div class="card-body">
+                            <h4 class="card-title">
+                            </h4>
+                            <small class="card-rating">
+                              <span id="review"></span>
+                              <span class="fa fa-star" id="s1"></span>
+                              <span class="fa fa-star" id="s2"></span>
+                              <span class="fa fa-star" id="s3"></span>
+                              <span class="fa fa-star" id="s4"></span>
+                              <span class="fa fa-star" id="s5"></span>
+                              <span id="votes"></span>
+                            </small>
+                            <h6 id="cuisine"></h6>
+                            <p class="card-text">
+                              <table class="table">
+                                <tr>
+                                  <td>Address</td>
+                                  <td id="address"></td>
+                                </tr>
+                                <tr>
+                                  <td>Homepage</td>
+                                  <td id="homepage"></td>
+                                </tr>
+                                <tr>
+                                  <td>Phone Number</td>
+                                  <td id="phone_number"></td>
+                                </tr>
+                              </table>
+                            </p>
+                          </div>
+                          <div class="card-footer text-center">
+                            <button class="btn text-danger">Favaorite</button>
+                            <button class="btn text-danger" onClick="" type="button" id="close">Close</button>
+                          </div>
+                        </div>
+                      </div>`
+      var $detail = $('#detail')
+      $detail.empty()
+      var $item = $(template)
+      console.log(result)
+      $item.find('.card-title').append(result.name)
+      $item.find('#review').append(result.user_rating.aggregate_rating)
+      for (var i = 0; i < Math.floor(result.user_rating.aggregate_rating); i++){
+        $item.find(`#s${i+1}`).addClass("checked")  
+      }
+      $item.find('#votes').append(`(${result.user_rating.votes})`)
+      $item.find('#cuisine').append(`${result.cuisines}`)
+      $item.find('#address').append(`${result.location.address}`)
+      var link = `<a href="${result.url}">${result.name}</a>`
+      $item.find('#homepage').append(link)
+      $item.find('#close').attr('onClick',`closeDetail()`)
+      $item.find('#phone_number').append(`${result.phone_numbers}`)
+      $item.find('.card-img').attr('src',result.featured_image)
+      newLocation(Number(result.location.latitude),Number(result.location.longitude))
+      calcRoute(Number(result.location.latitude),Number(result.location.longitude))
+      $detail.append($item)
+    })
+    .fail(function(err){
+      console.log(err)
+    })
+    
+  }
 
   function loadZomato(){
     var template = `
